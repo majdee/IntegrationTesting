8db2c23cc0b5fb3802f95dedacf5cb4d7f4b9392
smajdee.h@gmail.com <majdeZOA>
2019-12-08 22:26:11 +0200

    Add a test

--- UserManagment/UserManagementApiIntergrationTests/TestStartup.cs
+++ UserManagment/UserManagementApiIntergrationTests/TestStartup.cs
@@ -1,9 +1,34 @@
-﻿using UserManagementAPI;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using UserManagementAPI;
 
 namespace UserManagementApiIntegrationTests
 {
     public class TestStartup : Startup
     {
 
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+
+            AddInMemoryDbContext(services);
+        }
+
+        private void AddInMemoryDbContext(IServiceCollection services)
+        {
+            // Remove the app's ApplicationDbContext registration.
+            var descriptor =
+                services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<UserManagementDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Add ApplicationDbContext using an in-memory database for testing.
+            services.AddDbContext<UserManagementDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: $"InMemoryDbForTesting"));
+        }
     }
 }
--- UserManagment/UserManagment/Startup.cs
+++ UserManagment/UserManagment/Startup.cs
@@ -12,7 +12,7 @@ namespace UserManagementAPI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public virtual void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<UserManagementDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
